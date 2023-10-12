7c63d7a28f94632df4a661b1d5e59f0ca80562ff
Gennarro@yandex.ru <Gennarro@yandex.ru>
2023-10-12 19:56:36 +0300

    add score

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 const ballVelocity = 10
 const ballSize = 20
+const gameOverLayout = document.querySelector('gameOverLayout')
 
 function collides(obj1, obj2) {
     return obj1.x < obj2.x + obj2.width &&
@@ -8,6 +9,8 @@ function collides(obj1, obj2) {
            obj1.y < obj2.y + obj2.height &&
            obj1.y + obj1.height > obj2.y;
   }
+
+
 
 class LeftDino {
     constructor(game){
@@ -30,6 +33,7 @@ class LeftDino {
 
         }
     draw(context){
+        context.fillStyle = "white";
         context.fillRect(this.x,this.y,this.width,this.height)
         context.drawImage(this.image,0,0,this.width,this.height,this.x,this.y,this.width,this.height)
     }
@@ -48,6 +52,7 @@ class RightDino {
 
         }
     draw(context){
+        context.fillStyle = "white";
         context.fillRect(this.x,this.y,this.width,this.height)
         context.drawImage(this.image,0,0,this.width,this.height,this.x,this.y,this.width,this.height)
     }
@@ -68,16 +73,24 @@ class Ball {
         this.y -=this.velocityY
 
         if (this.y < ballSize) {
-            this.y = ballSize;
-            this.velocityY *= -1;
+            this.y = ballSize
+            this.velocityY *= -1
         }else if (this.y + ballSize > this.game.height - ballSize) {
-            this.y = this.game.height - ballSize * 2;
-            this.velocityY *= -1;
+            this.y = this.game.height - ballSize * 2
+            this.velocityY *= -1
         }
 
         if ( (this.x < 0 || this.x > this.game.width)) {
             this.x = this.game.width / 2;
             this.y = this.game.height / 2;
+            if(this.x < 0){
+                if(this.game.leftDinoScore==3) this.game.stopGame()
+                else this.game.leftDinoScore++
+            }else{
+                if(this.game.rightDinoScore==3) this.game.stopGame()
+                else this.game.rightDinoScore++
+            }
+           
         }
 
         if (collides(this, this.game.leftDino)) {
@@ -90,10 +103,21 @@ class Ball {
 
     }
     draw(context){
+        context.fillStyle = "black";
         context.fillRect(this.x,this.y,this.width,this.height)
-        
     }
 }
+
+class Score {
+    constructor (game) {
+        this.game = game
+    }
+    draw(ctx){
+        ctx.fillStyle = "rgba(100,100,100)";
+        ctx.fillText(this.game.leftDinoScore,this.game.width*0.25,this.game.height*0.25);
+        ctx.fillText(this.game.rightDinoScore,this.game.width*0.75,this.game.height*0.25);
+    }
+} 
 
 class InputHandler {
     constructor () {
@@ -121,18 +145,24 @@ class InputHandler {
 window.addEventListener('load', ()=>{
     const canvas = document.getElementById('pCanvas')
 
-    canvas.width = 900
-    canvas.height = 600
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
     const ctx = canvas.getContext('2d')
+    ctx.font = "200px 'Roboto', Arial";
+    ctx.textAlign = "center";
 
         class Game {
             constructor(width,height){
                 this.width = width
                 this.height = height
+                this.leftDinoScore = 0
+                this.rightDinoScore = 0
                 this.leftDino = new LeftDino(this)
                 this.rightDino = new RightDino(this)
                 this.ball = new Ball(this)
                 this.input = new InputHandler()
+                this.score = new Score(this)
+                this.playOn = true
             }
             update(){
                 this.leftDino.update(this.input.keys)
@@ -143,7 +173,16 @@ window.addEventListener('load', ()=>{
             draw(context){
                 this.leftDino.draw(context)
                 this.rightDino.draw(context)
+                this.score.draw(context)
                 this.ball.draw(context)
+            }
+            startGame(){
+                this.playOn = true
+                animate()
+            }
+            stopGame(){
+                this.playOn = false
+                gameOverLayout.classList.add('active');
             }
         }
 
@@ -152,7 +191,9 @@ window.addEventListener('load', ()=>{
             ctx.clearRect(0,0,canvas.width,canvas.height)
             game.update()
             game.draw(ctx)
-            requestAnimationFrame(animate)
+            if(game.playOn){
+                requestAnimationFrame(animate)
+            }
         }
         animate()
     }
